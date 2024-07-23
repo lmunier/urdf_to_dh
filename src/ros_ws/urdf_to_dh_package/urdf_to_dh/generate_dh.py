[WIP] Working on theta computation implementation. Add the axis set for a fixed joint.

src/ros_ws/urdf_to_dh_package/urdf_to_dh/generate_dh.py
@@ -55,8 +55,8 @@ class GenerateDhParams(rclpy.node.Node):
         self.urdf_joints = {}
         self.urdf_links = {}
         self.urdf_tree_nodes = []
-        self.dh_params = {}
         self.root_link = None
+        self.reference_axis = None
 
         self.urdf_file = self.get_parameter(
             'urdf_file'
@@ -110,7 +110,7 @@ class GenerateDhParams(rclpy.node.Node):
         # Find root link
         num_nodes_no_parent = 0
         for n in self.urdf_tree_nodes:
-            if n.parent == None:
+            if n.is_root:
                 num_nodes_no_parent += 1
                 self.root_link = n
 
@@ -127,6 +127,17 @@ class GenerateDhParams(rclpy.node.Node):
             print(f"\n")
         else:
             print("Error: Should only be one root link")
+
+        # Define axis for fixed joints
+        for n in LevelOrderIter(self.root_link):
+            if n.type == 'joint':
+                joint = self.urdf_joints[n.id]
+                if self.reference_axis is None and joint['type'] == 'revolute':
+                    self.reference_axis = uh.get_reference_axis(joint)
+                    print(f"Reference Axis: {self.reference_axis}")
+
+                if joint['type'] == 'fixed' and joint['axis'] == None:
+                    joint['axis'] = uh.get_axis(n, self.urdf_joints)
 
     def calculate_tfs(self):
         """ Calculate the transformation matrices for each link in both the local and the world frame. """
@@ -174,6 +185,13 @@ class GenerateDhParams(rclpy.node.Node):
                 )
 
                 # DH parameters
+                theta_val = np.arccos(np.dot(
+                    self.reference_axis, common_normal
+                ))
+                print(
+                    f"Theta: {theta_val} Reference Axis: {self.reference_axis} Common Normal: {common_normal}")
+                self.reference_axis = kh.inv_tf(tf)[0:3, 0:3] @ common_normal
+
                 alpha_val = np.arccos(
                     np.dot(parent_joint_axis, joint_axis_in_parent)
                 )
@@ -188,7 +206,9 @@ class GenerateDhParams(rclpy.node.Node):
                     a_val = np.dot(tf[0:3, 3], common_normal)
 
                 self.urdf_links[n.id]['dh_found'] = True
-                self.dh_params[n.id] = [d_val, 0, a_val, alpha_val]
+                self.urdf_joints[n.id] = np.array(
+                    [d_val, theta_val, a_val, alpha_val]
+                )
 
     def display_dh_params(self):
         """ Calculate the DH parameters for each joint. """
@@ -201,7 +221,7 @@ class GenerateDhParams(rclpy.node.Node):
                         urdf_node.parent.id,
                         urdf_node.parent.parent.id,
                         urdf_node.id
-                    ] + self.dh_params[urdf_node.id]
+                    ] + self.urdf_joints[urdf_node.id].tolist()
                 )
 
         pd_frame = pd.DataFrame(

src/ros_ws/urdf_to_dh_package/urdf_to_dh/urdf_helpers.py
@@ -7,6 +7,8 @@
 import xml.etree.ElementTree as ET
 import numpy as np
 
+from anytree import AnyNode
+
 
 def get_urdf_root(urdf_file: str) -> ET.Element:
     """Parse a URDF for joints.
@@ -25,6 +27,75 @@ def get_urdf_root(urdf_file: str) -> ET.Element:
     return tree.getroot()
 
 
+def get_reference_axis(joint: dict, epsilon: float = 1e-10) -> np.ndarray:
+    """Extracts the reference axis from the joint.
+
+    Args:
+        joint: The joint element to extract the reference axis from.
+        epsilon: The tolerance for floating point comparisons.
+
+    Returns:
+        reference_axis: The reference axis of the URDF.
+    """
+    x_axis = np.array([1, 0, 0])
+    y_axis = np.array([0, 1, 0])
+    z_axis = np.array([0, 0, 1])
+
+    if np.array_equal(joint['axis'], x_axis):
+        return y_axis
+    elif np.array_equal(joint['axis'], y_axis):
+        return z_axis
+    elif np.array_equal(joint['axis'], z_axis):
+        return x_axis
+    else:
+        # Check if the input vector is close to zero to avoid division by zero
+        if np.linalg.norm(joint) < epsilon:
+            raise ValueError("Input vector is too close to zero.")
+
+        if not np.isclose(joint[0], 0) or not np.isclose(joint[1], 0):
+            return np.cross(joint, x_axis)
+        else:
+            return np.cross(joint, y_axis)
+
+
+def get_axis(node: AnyNode, joints: dict, direction: str = 'child') -> np.ndarray:
+    """Extracts the axis of rotation from next or previous joint element.
+
+    Args:
+        node: The joint node to set axis.
+        joints: The dictionary containing the joint info.
+        direction: The direction to extract the axis from.
+
+    Returns:
+        axis: The axis of rotation of the joint.
+    """
+    next_node = None
+    next_direction = direction
+
+    # Return the axis if it is already set
+    if joints[node.id]['axis'] is not None:
+        return joints[node.id]['axis']
+
+    # Check if it is the start or the end of the tree
+    if node.children[0].is_leaf:
+        next_direction = 'parent'
+    elif node.parent.is_root:
+        next_direction = 'child'
+
+    # Store the next node to extract the axis from
+    if next_direction == 'child':
+        next_node = node.children[0].children[0]
+    elif next_direction == 'parent':
+        next_node = node.parent.parent
+    else:
+        print(
+            f"ERROR: {next} is not a known next joint direction. Should be ['child' or 'parent']."
+        )
+
+    # Recursive call
+    return get_axis(next_node, joints, direction=next_direction)
+
+
 def process_joint(joint: ET.Element) -> tuple:
     """Extracts the relevant joint info into a dictionary.
 
@@ -35,13 +106,14 @@ def process_joint(joint: ET.Element) -> tuple:
         joint_name: The name of the joint.
         joint_info: A dictionary containing the joint info.
     """
-    axis = np.array([0, 0, 1])
+    axis = None
     xyz = np.zeros(3)
     rpy = np.zeros(3)
     parent_link = ''
     child_link = ''
 
     joint_name = joint.get('name')
+    joint_type = joint.get('type')
 
     for child in joint:
         if child.tag == 'axis':
@@ -54,5 +126,12 @@ def process_joint(joint: ET.Element) -> tuple:
         elif child.tag == 'child':
             child_link = child.get('link')
 
-    return joint_name, {'axis': axis, 'xyz': xyz, 'rpy': rpy, 'parent': parent_link, 'child': child_link,
-                        'dh': np.zeros(4), 'type': joint.get('type')}
+    return joint_name, {
+        'axis': axis,
+        'xyz': xyz,
+        'rpy': rpy,
+        'parent': parent_link,
+        'child': child_link,
+        'dh': np.zeros(4),
+        'type': joint_type
+    }